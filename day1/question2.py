37c5f3f3ec9b01f9cba26aa994740811ca557ef6
unknown <nahomkeneni4@gmail.com>
2025-04-23 01:41:42 -0700

    questions_and_answers

---
+++ day1/question2.py
@@ -0,0 +1,28 @@
+# (This problem is an interactive problem.)
+
+# You may recall that an array arr is a mountain array if and only if:
+
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+# Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target. If such an index does not exist, return -1.
+
+# You cannot access the mountain array directly. You may only access the array using a MountainArray interface:
+
+# MountainArray.get(k) returns the element of the array at index k (0-indexed).
+# MountainArray.length() returns the length of the array.
+# Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        locations = [0] * 1001
+        for numPassengers, start, end in trips:
+            locations[start] += numPassengers
+            locations[end] -= numPassengers
+        
+        for numPassengers in locations:
+            capacity -= numPassengers
+            if capacity < 0: return False
+        
+        return True
